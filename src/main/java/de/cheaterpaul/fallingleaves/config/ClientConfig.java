ebfa9fbc6e44382265e279daf27e6ac9343f6d2c
cheaterpaul <dev@paube.de>
2021-03-31 13:26:42 +0200

    add config comment

--- src/main/java/de/cheaterpaul/fallingleaves/config/ClientConfig.java
+++ src/main/java/de/cheaterpaul/fallingleaves/config/ClientConfig.java
@@ -13,12 +13,12 @@ public class ClientConfig {
     public final ForgeConfigSpec.IntValue minimumFreeSpaceBelow;
 
     public ClientConfig(ForgeConfigSpec.Builder builder) {
-        leafSize = builder.defineInRange("leafSize", 4, 1, 20);
-        leafLifespan = builder.defineInRange("leafLifespan", 200, 100, 2000);
-        leafSpawnRate = builder.defineInRange("leafSpawnRate", 10, 0, 10000);
-        coniferLeafSpawnRate = builder.defineInRange("coniferLeafSpawnRate", 2, 0, 10000);
-        dropFromPlayerPlacedBlocks = builder.define("dropFromPlayerPlacedBlocks", true);
-        minimumFreeSpaceBelow = builder.defineInRange("minimumFreeSpaceBelow", 1, 1, 20);
+        leafSize = builder.comment("modifies the size of the leaves").defineInRange("leafSize", 4, 1, 20);
+        leafLifespan = builder.comment("modifies how long it takes for the leaves to disappear").defineInRange("leafLifespan", 200, 100, 2000);
+        leafSpawnRate = builder.comment("modifies the amount of leaves that are spawning").defineInRange("leafSpawnRate", 10, 0, 10000);
+        coniferLeafSpawnRate = builder.comment("modifier the amount of leaves are spawning from conifer trees").defineInRange("coniferLeafSpawnRate", 2, 0, 10000);
+        dropFromPlayerPlacedBlocks = builder.comment("weather player placed blocks should also drop leaves").define("dropFromPlayerPlacedBlocks", true);
+        minimumFreeSpaceBelow = builder.comment("how much room below the leaves block is needed for the leaves to spawn").defineInRange("minimumFreeSpaceBelow", 1, 1, 20);
     }
 
 }
