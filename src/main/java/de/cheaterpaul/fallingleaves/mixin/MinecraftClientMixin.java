573e41417abdbc398f38c2d69e07fef36730a60c
cheaterpaul <dev@paube.de>
2024-05-10 19:06:16 +0200

    update to forge 1.20.6

--- src/main/java/de/cheaterpaul/fallingleaves/mixin/MinecraftClientMixin.java
+++ src/main/java/de/cheaterpaul/fallingleaves/mixin/MinecraftClientMixin.java
@@ -2,6 +2,7 @@ package de.cheaterpaul.fallingleaves.mixin;
 
 import de.cheaterpaul.fallingleaves.util.Wind;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.ReceivingLevelScreen;
 import net.minecraft.client.multiplayer.ClientLevel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Minecraft.class)
 public class MinecraftClientMixin {
 
-    @Inject(method = "setLevel(Lnet/minecraft/client/multiplayer/ClientLevel;)V", at = @At("HEAD"))
-    public void joinWorld(ClientLevel world, CallbackInfo ci) {
+    @Inject(method = "setLevel", at = @At("HEAD"))
+    public void joinWorld(ClientLevel p_91157_, ReceivingLevelScreen.Reason p_335893_, CallbackInfo ci) {
         Wind.init();
     }
 
